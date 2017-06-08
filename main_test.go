89c6839e67e9f6e969a1bf64ef7dba9a498c75ec
mchalczynski <marcin.chalczynski@rndity.com>
2017-06-08 12:56:18 +0200

    main, main_test: add testing function for running acceptance tests
    this is the 'main' that will be called during acceptance tests.
    
    note a workaround for flags vs urfave/cli conflicts:
    during tests, we have to pass several golang flags, incompatible with cli;
    all cli-specific arguments will then be passed as the -cli-args flag.
    to enable this behavior, the main() logic is factored out to a parametrized
    doMain(args) func.
    
    this is not strictly necessary for now, but might become handy in case we want to
    pass custom configs/params. the pattern is used in all new services/tests.
    
    Issues: MEN-1223
    
    Changelog: None
    
    Signed-off-by: mchalczynski <marcin.chalczynski@rndity.com>

---
+++ main_test.go
@@ -0,0 +1,61 @@
+// Copyright 2016 Mender Software AS
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+package main
+
+import (
+	"flag"
+	"os"
+	"os/signal"
+	"strings"
+	"testing"
+
+	logt "github.com/mendersoftware/go-lib-micro/log/testing"
+)
+
+var runAcceptanceTests bool
+
+// used for parsing '-cli-args' for urfave/cli when running acceptance tests
+// this is because of a conflict between urfave/cli and regular go flags required for testing (can't mix the two)
+var cliArgsRaw string
+
+func init() {
+	logt.MaybeDiscardLogs()
+
+	flag.BoolVar(&runAcceptanceTests, "acceptance-tests", false, "set flag when running acceptance tests")
+	flag.StringVar(&cliArgsRaw, "cli-args", "", "for passing urfave/cli args (single string) when golang flags are specified (avoids conflict)")
+
+	flag.Parse()
+}
+
+func TestRunMain(t *testing.T) {
+	if !runAcceptanceTests {
+		t.Skip()
+	}
+
+	// parse '-cli-args', remember about binary name at idx 0
+	var cliArgs []string
+
+	if cliArgsRaw != "" {
+		cliArgs = []string{os.Args[0]}
+		splitArgs := strings.Split(cliArgsRaw, " ")
+		cliArgs = append(cliArgs, splitArgs...)
+	}
+
+	go doMain(cliArgs)
+
+	stopChan := make(chan os.Signal)
+	signal.Notify(stopChan, os.Interrupt)
+
+	<-stopChan
+}
