43f769ec54ebe5f76d6a624646dc5968b2b13cfb
mchalczynski <marcin.chalczynski@rndity.com>
2017-07-05 13:41:50 +0200

    tests/client: base api client
    Issues: MEN-1282
    
    Changelog: None
    
    Signed-off-by: mchalczynski <marcin.chalczynski@rndity.com>

---
+++ tests/tests/client.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+# Copyright 2016 Mender Software AS
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        https://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+import os.path
+import logging
+import pytest
+from bravado.swagger_model import load_file
+from bravado.client import SwaggerClient, RequestsClient
+import subprocess
+
+
+class ApiClient:
+    config = {
+        'also_return_response': True,
+        'validate_responses': True,
+        'validate_requests': False,
+        'validate_swagger_spec': False,
+        'use_models': True,
+    }
+
+    log = logging.getLogger('client.ApiClient')
+    # override spec_option for internal vs management clients
+    spec_option = 'internal-spec'
+    api_url = "http://%s/api/0.1.0/" % \
+              pytest.config.getoption("host")
+
+    def make_api_url(self, path):
+        return os.path.join(self.api_url,
+                            path if not path.startswith("/") else path[1:])
+
+    def setup_swagger(self):
+        self.http_client = RequestsClient()
+        self.http_client.session.verify = False
+
+        spec = pytest.config.getoption(self.spec_option)
+        self.client = SwaggerClient.from_spec(load_file(spec),
+                                              config=self.config,
+                                              http_client=self.http_client)
+        self.client.swagger_spec.api_url = self.api_url
+
+    def __init__(self):
+        self.setup_swagger()
