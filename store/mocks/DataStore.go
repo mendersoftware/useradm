ab99238bc6020ea0b54829096f53584461bc13cb
mchalczynski <marcin.chalczynski@rndity.com>
2018-05-28 11:52:23 +0200

    store: get settings
    changelog: none
    
    Signed-off-by: mchalczynski <marcin.chalczynski@rndity.com>

--- store/mocks/DataStore.go
+++ store/mocks/DataStore.go
@@ -79,6 +79,29 @@ func (_m *DataStore) DeleteUser(ctx context.Context, id string) error {
 	return r0
 }
 
+// GetSettings provides a mock function with given fields: ctx
+func (_m *DataStore) GetSettings(ctx context.Context) (map[string]interface{}, error) {
+	ret := _m.Called(ctx)
+
+	var r0 map[string]interface{}
+	if rf, ok := ret.Get(0).(func(context.Context) map[string]interface{}); ok {
+		r0 = rf(ctx)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(map[string]interface{})
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context) error); ok {
+		r1 = rf(ctx)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // GetTokenById provides a mock function with given fields: ctx, id
 func (_m *DataStore) GetTokenById(ctx context.Context, id string) (*jwt.Token, error) {
 	ret := _m.Called(ctx, id)
