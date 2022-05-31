116970914cb90ef4a7de64044caa89c43e5c4b24
Krzysztof Jaskiewicz <krzysztof.jaskiewicz@northern.tech>
2022-05-31 11:18:17 +0200

    chore(model): introduce Personal Access Token related objects
    Signed-off-by: Krzysztof Jaskiewicz <krzysztof.jaskiewicz@northern.tech>

---
+++ model/token.go
@@ -0,0 +1,72 @@
+// Copyright 2022 Northern.tech AS
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+package model
+
+import (
+	"encoding/json"
+	"time"
+
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/mendersoftware/go-lib-micro/mongo/oid"
+
+	"github.com/mendersoftware/useradm/jwt"
+)
+
+type TokenRequest struct {
+	Name      *string `json:"name"`
+	ExpiresIn int64   `json:"expires_in"`
+}
+
+func (tr TokenRequest) Validate() error {
+	return validation.ValidateStruct(&tr,
+		validation.Field(&tr.Name, validation.Required, lessThan4096),
+		validation.Field(
+			&tr.ExpiresIn, validation.Required, validation.Min(1), validation.Max(31536000)))
+}
+
+type PersonalAccessToken struct {
+	// system-generated user ID
+	ID oid.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	// token name
+	Name *string `json:"name,omitempty" on:"name,omitempty"`
+	// timestamp of the last usage
+	LastUsed *time.Time `json:"last_used,omitempty" bson:"last_used,omitempty"`
+	// the absolute time when the token expires.
+	ExpirationDate jwt.Time `json:"expiration_date,omitempty" bson:"exp,omitempty"`
+}
+
+type apiToken struct {
+	// system-generated user ID
+	ID oid.ObjectID `json:"id,omitempty"`
+	// token name
+	Name *string `json:"name,omitempty"`
+	// timestamp of the last usage
+	LastUsed *time.Time `json:"last_used,omitempty"`
+	// the absolute time when the token expires.
+	ExpirationDate time.Time `json:"expiration_date,omitempty"`
+}
+
+func newApiToken(t PersonalAccessToken) apiToken {
+	return apiToken{
+		t.ID,
+		t.Name,
+		t.LastUsed,
+		t.ExpirationDate.Time,
+	}
+}
+
+func (t PersonalAccessToken) MarshalJSON() ([]byte, error) {
+	return json.Marshal(newApiToken(t))
+}
