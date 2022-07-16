e96ed957022ec05c52bef2efbee89494f5dbaca2
Fabio Tranchitella <fabio.tranchitella@northern.tech>
2022-07-16 09:21:33 +0200

    fix: do not create tenant-specific databases when creating tenants
    Changelog: None
    Ticket: None
    
    Signed-off-by: Fabio Tranchitella <fabio.tranchitella@northern.tech>

--- tests/tests/test_api_internal.py
+++ tests/tests/test_api_internal.py
@@ -35,8 +35,8 @@ class TestInternalApiTenantCreate:
         _, r = api_client_int.create_tenant("foobar")
         assert r.status_code == 201
 
-        assert "useradm-foobar" in clean_db.list_database_names()
-        assert "migration_info" in clean_db["useradm-foobar"].list_collection_names()
+        assert "useradm" in clean_db.list_database_names()
+        assert "migration_info" in clean_db["useradm"].list_collection_names()
 
     def test_create_twice(self, api_client_int, clean_db):
 
